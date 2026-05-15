fc3459529882c7f9482f7eb4c9fe8717e08a8014
onlysheluvsrocket <andrejbulgakov999v@gmail.com>
2026-05-15 20:13:18 +0800

    Update factorial.cpp

--- lab1/src/factorial.cpp
+++ lab1/src/factorial.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+unsigned long long factorial(int n) {
+    if (n <= 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+int main() {
+    int number;
+    cout << "Enter a positive integer: ";
+    cin >> number;
+    
+    if (number < 0) {
+        cout << "Factorial is not defined for negative numbers." << endl;
+    } else {
+        cout << "Factorial of " << number << " = " << factorial(number) << endl;
+    }
+    return 0;
+}
